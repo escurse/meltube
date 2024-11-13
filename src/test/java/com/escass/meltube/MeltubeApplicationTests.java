72bff5b69e7abd20609d5e1b772fb9b8e4b3ac62
escurse <av1967@naver.com>
2024-11-13 15:58:52 +0900

    melon

---
+++ src/test/java/com/escass/meltube/MeltubeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.escass.meltube;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeltubeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
