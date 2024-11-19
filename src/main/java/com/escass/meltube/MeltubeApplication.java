308170d7fbae85edc9c3758393281d27bb3bf182
escurse <av1967@naver.com>
2024-11-19 12:31:34 +0900

    meltube

--- src/main/java/com/escass/meltube/MeltubeApplication.java
+++ src/main/java/com/escass/meltube/MeltubeApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class) // security 쓰면 강제로 /login 으로 맵핑 시켜버리기 때문에 막기 위한 용도
 public class MeltubeApplication {
     public static void main(String[] args) {
         SpringApplication.run(MeltubeApplication.class, args);
